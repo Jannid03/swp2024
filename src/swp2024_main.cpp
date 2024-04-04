35a136c38271e07233955b81a7a8fb0ba42ae839
Jannid03 <jannikdubrau@gmx.de>
2024-04-04 14:10:59 +0200

    K, Window kostumisierbar

--- src/swp2024_main.cpp
+++ src/swp2024_main.cpp
@@ -9,18 +9,21 @@
 #include <algorithm>
 #include <ranges>
 #include <map>
+#include <limits.h>
 
 using namespace seqan3::literals;
 
 
-//TO DO: 1. to map conversion
-// Wenn nicht -> System (Input1 +, Input2 -, dann verrechnen)
+//TO DO: 
+// 1. K mer Customizing
+// 2. File input 
+// 3. Window size custom
 
-//2. Jaccard Funktion
-//3. Ausgabe
 
 struct eingabe {
     std::string modus{"k"};
+    uint8_t k {5};
+    size_t window {8};
     std::vector<std::filesystem::path> file{};
 };
 
@@ -75,8 +78,15 @@ void intialize_parser (sharg::parser & parser, eingabe & in) {
                                         .long_id = "mode", 
                                         .description ="Auswahl der Methode",
                                         .validator = mode_validator});
-    int test {0};
-    parser.add_option(test, sharg::config {.short_id = 't', .long_id = "Test", .hidden = true});
+
+    sharg::arithmetic_range_validator kmere_validator{1, 58};
+    parser.add_option(in.k, sharg::config {.short_id = 'k', .long_id = "kmer",
+                                            .description = "Länge der Kmere",
+                                            .required = true,
+                                            .validator = kmere_validator});
+    
+    parser.add_option(in.window, sharg::config {.short_id = 'w', .long_id = "window",
+                                            .description = "Länge des Windows"});
     /*sharg::input_file_validator my_file_ext_validator{{"fa", "fasta"}};
     parser.add_option(in.file, sharg::config{   .short_id = 'i',
                                                 .long_id = "input",
@@ -85,51 +95,52 @@ void intialize_parser (sharg::parser & parser, eingabe & in) {
     
 }
 
-void kmere (std::vector<seqan3::dna4> & seq1, std::vector<seqan3::dna4> & seq2) {
+void kmere (std::vector<seqan3::dna4> & seq1, std::vector<seqan3::dna4> & seq2, uint8_t km) {
 
-    auto kmere_seq1 = seq1 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{3}});
+    auto kmere_seq1 = seq1 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{km}});
     seqan3::debug_stream << kmere_seq1 << '\n';
 
-    auto kmere_seq2 = seq2 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{3}});
+    auto kmere_seq2 = seq2 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{km}});
     seqan3::debug_stream << kmere_seq2 << '\n';
 
     double kmere_jac = jaccard_index(kmere_seq1, kmere_seq2);
     std::cout << kmere_jac << std::endl;
 }
 
-void mini (std::vector<seqan3::dna4> & seq1, std::vector<seqan3::dna4> & seq2) {
+void mini (std::vector<seqan3::dna4> & seq1, std::vector<seqan3::dna4> & seq2, uint8_t km, uint32_t w) {
     uint64_t seed = 0x8F3F73B5CF1C9ADE;
-    auto mini_seq1 = seq1 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{3}})
+    uint32_t window = w - km + 1;
+    auto mini_seq1 = seq1 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{km}})
                             | std::views::transform([seed](uint64_t i) {return i^seed;})
-                            | seqan3::views::minimiser (3);
+                            | seqan3::views::minimiser (window);
     seqan3::debug_stream << mini_seq1 << '\n';
 
-    auto mini_seq2 = seq2 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{3}})
+    auto mini_seq2 = seq2 | seqan3::views::kmer_hash(seqan3::shape{seqan3::ungapped{km}})
                             | std::views::transform([seed](uint64_t i) {return i^seed;})
-                            | seqan3::views::minimiser (3);
+                            | seqan3::views::minimiser (window);
     seqan3::debug_stream << mini_seq2 << '\n';
 
     double mini_jac = jaccard_index(mini_seq1, mini_seq2);
     std::cout << mini_jac << std::endl;
 }
 
-void run_program (char modus) {
+void run_program (eingabe & in) {
 
-
+    char modus = in.modus[0];
     std::vector<seqan3::dna4> seq1 {"AGCTGTCGAAAGTCGAAAT"_dna4};
     std::vector<seqan3::dna4> seq2 {"CATGATGTCACTGATCGTA"_dna4};
 
     if (modus == 'k') {
 
-        kmere(seq1, seq2);
+        kmere(seq1, seq2, in.k);
     }
     else if (modus == 'm') {
 
-        mini(seq1, seq2);
+        mini(seq1, seq2, in.k, in.window);
     }
     else {
-        kmere(seq1,seq2);
-        mini(seq1,seq2);
+        kmere(seq1,seq2, in.k);
+        mini(seq1,seq2, in.k, in.window);
     }
     
 }
@@ -150,6 +161,6 @@ int main(int argc, char** argv) {
         return -1;
     }
  
-    run_program(in.modus[0]);
+    run_program(in);
 
 }
